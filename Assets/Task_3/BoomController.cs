320150240a0951b444b45eb77cfd034261fc8649
Zh-AZ <zhaparovaziz03@gmail.com>
2025-02-06 00:14:51 +0800

    Boom

---
+++ Assets/Task_3/BoomController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoomController : MonoBehaviour
+{
+    [SerializeField] private float timeToExplosion;
+    [SerializeField] private float power;
+    [SerializeField] private float radius;
+    private Rigidbody[] blocks;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Rigidbody[] blocks = FindObjectsOfType<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToExplosion -= Time.deltaTime;
+
+        if (timeToExplosion <= 0 )
+        {
+            Boom();
+        }
+    }
+
+    private void Boom()
+    {
+        Rigidbody[] blocks = FindObjectsOfType<Rigidbody>();
+
+        foreach (Rigidbody b in blocks )
+        {
+            float distance = Vector3.Distance(transform.position, b.transform.position);
+
+            if (distance < radius)
+            {
+                Vector3 direction = b.transform.position - transform.position;
+                b.AddForce(direction.normalized * power * (radius - distance), ForceMode.Impulse);
+            }
+        }
+
+        timeToExplosion = 3;
+    }
+}
